560b129d2fa90904001fb76d5e737b762b71f7a7
Gabriel-jimeneze <78098584+Gabriel-jimeneze@users.noreply.github.com>
2022-04-24 15:11:28 -0500

    Update Proyecto 3 Codigo G.py

--- Proyecto 3 Codigo G.py
+++ Proyecto 3 Codigo G.py
@@ -7,3 +7,20 @@ Created on Sun Apr 24 14:25:54 2022
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+datos = pd.read_csv('')
+datos.head()
+predictores= datos.keys()
+predictores= predictores.drop("")
+objetivo = ""
+X=datos[predictores]
+Y=datos[objetivo]
+
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+X_entreno, Y_entreno, X_testeo, Y_testeo=  train_test_split(X,Y, test_size=0.3)
+GB = GradientBoostingRegressor(n_estimators=100, learning_rate=0.01)
+GB.fit(X_entreno, Y_entreno)
+r2_puntaje= r2_score(Y_entreno, GB.predict(X_entreno))
