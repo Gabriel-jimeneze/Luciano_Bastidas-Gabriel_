edb9fb09415f350e8e7a9b7ce7c909c92e4ab9bb
Lbasper <99780061+Lbasper@users.noreply.github.com>
2022-04-26 17:44:52 -0500

    Update Proyecto 3 Codigo doble.py

--- Proyecto 3 Codigo doble.py
+++ Proyecto 3 Codigo doble.py
@@ -34,13 +34,15 @@ b=datos3[llav3[2]]/datos3[llav3[3]]
 datos3=pd.concat([a,b],axis=1, join ='inner')
 
 
-datospredic = pd.read_csv("Porcen_est_doble_2022_1.csv",encoding='latin-1',delimiter=";").fillna(0)
+datospredic = pd.read_csv("Matriz doble programa.csv",encoding='latin-1',delimiter=";").fillna(0)
 llavpredic=datospredic.keys()
-sumar=datospredic[llavpredic[1]].sum()
-datospredic=datospredic[llavpredic[1]]/sumar
+
+programa=2
+sumar=datospredic[llavpredic[programa]].sum()
+datospredic=datospredic[llavpredic[programa]]/sumar
 
 # Columna 1_%doc,2_%maes,3_%esp/pre,4_%hombre_cat,5_%mujeres_cat,6_%hombre_plan,7_%mujer_plan
-X=pd.concat([datos1,datos2,datos3],axis=1, join ='inner')
+X=pd.concat([datos1,datos2,datos3],axis=1, join ='inner',ignore_index=True)
 
 
 Y=datospredic
@@ -59,7 +61,7 @@ sexto=[]
 septimo=[]
 for i in range(0,n_interracciones):
     
-    X_entreno,X_testeo, Y_entreno, Y_testeo=  train_test_split(X,Y, test_size=0.4)
+    X_entreno,X_testeo, Y_entreno, Y_testeo=  train_test_split(X,Y, test_size=0.7)
     GB = GradientBoostingRegressor(n_estimators=100, learning_rate=0.01)
     GB.fit(X_entreno, Y_entreno)
     r2_puntaje= r2_score(Y_entreno, GB.predict(X_entreno))
@@ -74,35 +76,36 @@ for i in range(0,n_interracciones):
     septimo.append((t[6]))
 finales=[]
 finales.append(np.mean(r2))
-finales.append([np.mean(primero),np.std(primero)])
-finales.append([np.mean(segundo),np.std(segundo)])
-finales.append([np.mean(tercero),np.std(tercero)])
-finales.append([np.mean(cuarto),np.std(cuarto)])
-finales.append([np.mean(quinto),np.std(quinto)])
-finales.append([np.mean(sexto),np.std(sexto)])
-finales.append([np.mean(septimo),np.std(septimo)])
+# finales.append([np.mean(primero),np.std(primero)])
+# finales.append([np.mean(segundo),np.std(segundo)])
+# finales.append([np.mean(tercero),np.std(tercero)])
+# finales.append([np.mean(cuarto),np.std(cuarto)])
+# finales.append([np.mean(quinto),np.std(quinto)])
+# finales.append([np.mean(sexto),np.std(sexto)])
+# finales.append([np.mean(septimo),np.std(septimo)])
 print(finales)
+print(llavpredic[programa])
 
-learning_rates = [0.01,0.1,1]
-n_estimators = np.arange(1,220,20)
+# learning_rates = [0.01,0.1,1]
+# n_estimators = np.arange(1,220,20)
 
-fig = plt.figure(figsize=(12,5))
+# fig = plt.figure(figsize=(12,5))
 
-for lr in learning_rates:
-    r2_test = []
-    r2_train = []
+# for lr in learning_rates:
+#     r2_test = []
+#     r2_train = []
 
-    for ne in n_estimators:
-        GB = GradientBoostingRegressor(n_estimators=ne, learning_rate=lr)
-        GB.fit(X_entreno, Y_entreno)
-        r2_test.append( r2_score(Y_testeo, GB.predict(X_testeo)) )
-        r2_train.append( r2_score(Y_entreno, GB.predict(X_entreno)) )
+#     for ne in n_estimators:
+#         GB = GradientBoostingRegressor(n_estimators=ne, learning_rate=lr)
+#         GB.fit(X_entreno, Y_entreno)
+#         r2_test.append( r2_score(Y_testeo, GB.predict(X_testeo)) )
+#         r2_train.append( r2_score(Y_entreno, GB.predict(X_entreno)) )
     
-    plt.subplot(1,2,1)
-    plt.plot(n_estimators,r2_test, label=f'lr:{lr}')
+#     plt.subplot(1,2,1)
+#     plt.plot(n_estimators,r2_test, label=f'lr:{lr}')
         
-    plt.subplot(1,2,2)
-    plt.plot(n_estimators,r2_train, label=f'lr:{lr}')
+#     plt.subplot(1,2,2)
+#     plt.plot(n_estimators,r2_train, label=f'lr:{lr}')
     
 
 plt.subplot(1,2,1)
