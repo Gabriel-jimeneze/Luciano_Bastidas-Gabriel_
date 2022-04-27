3eed56718cc63bcdb2b6117d16fa5d653fac2717
Lbasper <99780061+Lbasper@users.noreply.github.com>
2022-04-26 22:36:59 -0500

    Update Proyecto 3 Codigo doble.py

--- Proyecto 3 Codigo doble.py
+++ Proyecto 3 Codigo doble.py
@@ -28,16 +28,19 @@ datos2=pd.concat([a,b],axis=1, join ='inner').fillna(0)
 
 #sexo profesores de planta
 datos3 = pd.read_csv("Porcen_sexo_planta_2022_1.csv",encoding='latin-1',delimiter=";").fillna(0)
-llav3=datos3.keys()
-a=datos3[llav3[1]]/datos3[llav3[3]]
-b=datos3[llav3[2]]/datos3[llav3[3]]
-datos3=pd.concat([a,b],axis=1, join ='inner')
+# llav3=datos3.keys()
+# a=datos3[llav3[1]]/datos3[llav3[3]]
+# b=datos3[llav3[2]]/datos3[llav3[3]]
+# datos3=pd.concat([a,b],axis=1, join ='inner')
 
 
 datospredic = pd.read_csv("Matriz doble programa.csv",encoding='latin-1',delimiter=";").fillna(0)
 llavpredic=datospredic.keys()
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
-programa=2
+programa=6
 sumar=datospredic[llavpredic[programa]].sum()
 datospredic=datospredic[llavpredic[programa]]/sumar
 
@@ -47,35 +50,38 @@ X=pd.concat([datos1,datos2,datos3],axis=1, join ='inner',ignore_index=True)
 
 Y=datospredic
 
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
-n_interracciones= 50
-r2=[]
-primero=[]
-segundo=[]
-tercero=[]
-cuarto=[]
-quinto=[]
-sexto=[]
-septimo=[]
-for i in range(0,n_interracciones):
+
+
+# n_interracciones= 50
+# r2=[]
+# primero=[]
+# segundo=[]
+# tercero=[]
+# cuarto=[]
+# quinto=[]
+# sexto=[]
+# septimo=[]
+# from sklearn.tree import DecisionTreeRegressor
+# from sklearn.ensemble import AdaBoostRegressor
+# for i in range(0,n_interracciones):
     
-    X_entreno,X_testeo, Y_entreno, Y_testeo=  train_test_split(X,Y, test_size=0.7)
-    GB = GradientBoostingRegressor(n_estimators=100, learning_rate=0.01)
-    GB.fit(X_entreno, Y_entreno)
-    r2_puntaje= r2_score(Y_entreno, GB.predict(X_entreno))
-    t=GB.feature_importances_ *100
-    r2.append(r2_puntaje)
-    primero.append(t[0])
-    segundo.append(t[1])
-    tercero.append((t[2]))
-    cuarto.append(t[3])
-    quinto.append(t[4])
-    sexto.append(t[5])
-    septimo.append((t[6]))
-finales=[]
-finales.append(np.mean(r2))
+#     X_entreno,X_testeo, Y_entreno, Y_testeo=  train_test_split(X,Y, test_size=0.7)
+
+#     AdaB = AdaBoostRegressor(base_estimator=DecisionTreeRegressor(max_depth=3), n_estimators=11, learning_rate=0.01)
+#     AdaB.fit(X_entreno, Y_entreno)
+#     r2_puntaje= r2_score(Y_entreno, GB.predict(X_entreno))
+#     t=GB.feature_importances_ *100
+#     print(t)
+#     print(r2_puntaje)
+#     primero.append(t[0])
+#     segundo.append(t[1])
+#     tercero.append((t[2]))
+#     cuarto.append(t[3])
+#     quinto.append(t[4])
+#     sexto.append(t[5])
+#     septimo.append((t[6]))
+# finales=[]
+# finales.append([np.mean(r2),np.std(r2)])
 # finales.append([np.mean(primero),np.std(primero)])
 # finales.append([np.mean(segundo),np.std(segundo)])
 # finales.append([np.mean(tercero),np.std(tercero)])
@@ -83,8 +89,9 @@ finales.append(np.mean(r2))
 # finales.append([np.mean(quinto),np.std(quinto)])
 # finales.append([np.mean(sexto),np.std(sexto)])
 # finales.append([np.mean(septimo),np.std(septimo)])
-print(finales)
-print(llavpredic[programa])
+# print(llavpredic[programa])
+# print(finales)
+
 
 # learning_rates = [0.01,0.1,1]
 # n_estimators = np.arange(1,220,20)
@@ -108,20 +115,21 @@ print(llavpredic[programa])
 #     plt.plot(n_estimators,r2_train, label=f'lr:{lr}')
     
 
-plt.subplot(1,2,1)
-plt.grid()
-plt.legend()
-plt.xlabel('n_estimators')
-plt.ylabel('r2_test')
+# plt.subplot(1,2,1)
+# plt.grid()
+# plt.legend()
+# plt.xlabel('n_estimators')
+# plt.ylabel('r2_test')
 
-plt.subplot(1,2,2)
+# estim=["% doctorados","% maestria","%especializacion","%Hombres catedra","% Mujeres catedra","%Hombres planta","% Mujeres planta"]
+# plt.subplot(1,2,2)
 
-# Grafica las importancias en orden descendente
-ii = np.argsort(GB.feature_importances_)
+# # Grafica las importancias en orden descendente
+# ii = np.argsort(GB.feature_importances_)
 
-importances = GB.feature_importances_[ii]
-predictors = X.keys()[ii]
-plt.figure()
-a = pd.Series(importances, index=predictors)
-a.plot(kind='barh')
-plt.xlabel('Feature Importances')
+# importances = GB.feature_importances_[ii]
+# predictors = X.keys()[ii]
+# plt.figure()
+# a = pd.Series(importances, index=predictors)
+# a.plot(kind='barh')
+# plt.xlabel('Feature Importances')
