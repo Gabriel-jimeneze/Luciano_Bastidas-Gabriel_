1c69611591cdb31a0eb03173df02748ffb752196
Ingenieropola94 <72977129+Ingenieropola94@users.noreply.github.com>
2022-04-27 12:29:50 -0500

    Add files via upload

--- Proyecto 3 Codigo G.py
+++ Proyecto 3 Codigo G.py
@@ -8,6 +8,8 @@ Created on Sun Apr 24 18:39:39 2022
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import StrMethodFormatter
+
 
 #nivel profesores
 datos1 = pd.read_csv("Porcen_doc_2022_1.csv",encoding='latin-1',delimiter=";").fillna(0)
@@ -62,7 +64,21 @@ print(t)
 learning_rates = [0.01,0.1,1]
 n_estimators = np.arange(1,220,20)
 
-fig = plt.figure(figsize=(12,5))
+
+
+
+Anch    = 9
+Alto    = 15
+
+cm      = 1/2.54
+fig, (ax) = plt.subplots(1, 1, figsize=(Anch*cm, Alto*cm))
+
+AllRed  = np.array([[252,187,161],[251,106,74],[153,0,13]])/256
+
+AllRed    = np.array([[102,194,165],[252,141,98],[141,160,203]])/256 # Colores 3 choses
+
+i  = 0
+
 for lr in learning_rates:
     r2_test = []
     r2_train = []
@@ -73,27 +89,69 @@ for lr in learning_rates:
             r2_test.append( r2_score(Y_testeo, GB.predict(X_testeo)) )
             r2_train.append( r2_score(Y_entreno, GB.predict(X_entreno)) )
     
-    plt.subplot(1,2,1)
-    plt.plot(n_estimators,r2_test, label=f'lr:{lr}')
-        
-    plt.subplot(1,2,2)
-    plt.plot(n_estimators,r2_train, label=f'lr:{lr}')
-    plt.subplot(1,2,1)
-    plt.grid()
+    
+    paint = AllRed[i]
+    
+    plt.subplot(2,1,1)
+    plt.plot(n_estimators,r2_test, color = paint, label=f'lr:{lr}', linewidth = 3)
     plt.legend()
-    plt.xlabel('n_estimators')
-    plt.ylabel('r2_test')
-plt.figure()
+    plt.xlim([0,210])
+#    plt.ylim([-0.3,-0.1])
+    plt.ylabel(r'$r^2 test$')
+
+
+
+    plt.subplot(2,1,2)
+    plt.plot(n_estimators,r2_train, color = paint, label=f'lr:{lr}', linewidth = 3)
+    plt.xlim([0,210])
+#    plt.ylim([0,1.05])
+
+    plt.legend()
+    plt.xlabel('n: estimadores')
+    plt.ylabel(r'$r^2 train$')
+    
+    i      += 1
+
+
+
+Anch    = 9
+Alto    = 9
+
+fig, (ax) = plt.subplots(1, 1, figsize=(Anch*cm, Alto*cm))
+
 plt.scatter(X[4],Y,label="Reales")
+
 plt.scatter(np.array(X[4]).reshape(1,-1),np.array(GB.predict(X)),label="Predichos")
-plt.xlabel("Profesores hombres en catedra")
-plt.ylabel("Porcentaje de doble carrera")
+plt.xlabel("Profesores hombres en catedra [-]")
+plt.ylabel("Doble carrera [%]")
 plt.legend()
 
-#Grafica las importancias en orden descendente
+
+
+Anch    = 9
+Alto    = 9
+
+fig, (ax) = plt.subplots(1, 1, figsize=(Anch*cm, Alto*cm))
+
 importances = GB.feature_importances_*100
 predictors = X.keys()
-plt.figure()
+
 a = pd.Series([33.33303261,49.10290515,0.71836079,3.64632882,6.23594064,3.20792674,3.75550525], predictors)
 a.plot(kind='barh')
-plt.xlabel('Feature Importances')
+plt.xlabel('Importancia')
+plt.xlim ([0,50])
+
+plt.yticks([])
+
+plt.text(51, -0.1, 'Doctorado',weight="bold")
+plt.text(51, 0.9, 'Maestría',weight="bold")
+plt.text(51, 1.9, 'Esp / Preg ',weight="bold")
+plt.text(51, 2.9, 'Cat: Hombre',weight="bold")
+plt.text(51, 3.9, 'Cat: Mujer',weight="bold")
+plt.text(51, 4.9, 'Planta: Hombre',weight="bold")
+plt.text(51, 5.9, 'Planta: Mujer',weight="bold")
+
+ # Columna 1_%doc,2_%maes,3_%esp/pre,4_%hombre_cat,5_%mujeres_cat,6_%hombre_plan,7_%mujer_plan
+
+
+    
